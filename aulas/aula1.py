96c527a5bfa65c117cf6060547fadc95a7d7ab13
annajullisouza14-lab <https://github.com/annajullisouza14-lab/lab25.git>
2026-03-03 20:43:19 -0300

    teste

---
+++ aulas/aula1.py
@@ -0,0 +1,6 @@
+nome = input("digite seu nome.")
+idade = input("digite a sua idade")      
+email = input("digite seu email.")
+telefone = input("insira o seu número de telefone")
+
+print ("os dados da sua conta são:", nome, idade, email, telefone)
