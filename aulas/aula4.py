6d1ed21c8dc9726c7ea3be2c0d022c4c43ef887b
pedro henrique <gamerpedrohenrique2000@gmail.com>
2026-04-20 20:20:34 -0300

    atualizaco

---
+++ aulas/aula4.py
@@ -0,0 +1,9 @@
+soma = 0
+
+for i in range (3):
+    nota = int(input("Digite sua nota: "))
+    soma = soma + nota
+    print("Variavel com valor atual: ", nota)
+
+print("Variavel com valor atual: ", nota)
+print("A soma total é: ", soma)
---
+++ aulas/teste1.py
@@ -0,0 +1,11 @@
+cadastro = input ("Você possui um cadastro ? ") 
+email = str 
+senha = 0
+nome = 0
+login = ()
+
+if cadastro == "sim" :
+    print ("Faça seu login.",)
+    email = input ("Preencha seu email: ")
+    senha = input ("Preencha seu senha: ")
+
