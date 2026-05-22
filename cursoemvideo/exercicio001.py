12c8f2af7e742dce1c72d0b1e2f22e557e273066
pedro <gamerpedrohenrique2000@gmail.com>
2026-05-22 12:51:32 -0300

    1

---
+++ cursoemvideo/exercicio001.py
@@ -0,0 +1,3 @@
+# crie um programa que escreva olá mundo na tela.
+
+print("Olá, mundo!")
---
+++ exercicios/funcões.py
@@ -0,0 +1,8 @@
+numero1 = int(input("digite o primeiro numero da soma: "))
+numero2 = int(input("digite o segundo numero da soma: "))
+
+def soma(numero1 ,numero2):
+    resultado = numero1 + numero2
+    print(resultado)
+    
+soma(numero1, numero2) 
